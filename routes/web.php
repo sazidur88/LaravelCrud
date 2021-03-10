8d0c4142287dbb72a799feabdf9806bbc37cf47b
sazidur88 <sazidurrahmansadi@gmail.com>
2021-03-11 04:46:28 +0600

    Laravel project basic

---
+++ routes/web.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route :: get('sadi', function(){
+//     echo ("Hello World\n" );
+//     return ("Hello World");
+    
+// });
+
+// Route::get("ashraful",function(){
+//     return("I am your instructor");
+// });
+
+
+// Route::get('dev/{name?}/{job?}', function($name = null,$job = null){
+//     echo "<h1>My name is $name and I am a professional $job developer.</h1>";
+// });
+
+
+
+// //get, post, (put, patch), delete, resource 
+
+
+
+// // //Prefix route
+// Route::prefix('user') -> group(function(){
+//     Route::get('ashraful',function(){
+//     echo "I am your instructor";
+// });
+
+//     Route::get('sadi',function(){
+//     echo "I am a laravel developer";
+// });
+
+// });
+
+// //Name route
+// Route::get('ash', function(){
+//     echo "Ash route name route";
+// }) -> name('ash-name');
+
+
+// //Controller create
+// Route::get('student', 'StudentController@studentInfo');
+
+
+Route::get('test', function(){
+
+});
+
+
+// Route::get('student', 'StudentController@studentInfo');
+
+Route::resource('sadi', 'studentController');
+// Route::get('sadi{id}', 'studentController@edit');
